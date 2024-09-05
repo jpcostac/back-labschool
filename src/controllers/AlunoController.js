32ab28560f442f9077bd645a37d173a10d8df426
31032019 <jpcostac17@gmail.com>
2024-09-05 11:13:28 -0300

    Adicionando comentários no Aluno controller e service

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -3,12 +3,12 @@ const alunoService = require('../services/AlunoService')
 
 module.exports = {
     findAllAlunos: async (request, response) => {
-        let json = {error: "", result: []}
+        let json = {error: "", result: []}                  //Objeto json inicializado com duas chaves, error(vazia) e result(lista vazia)
 
-        let alunos = await alunoService.readAlunos()
+        let alunos = await alunoService.readAlunos()        //Função de consulta ao banco de dados
 
-        for(let aluno of alunos){
-            json.result.push({
+        for(let aluno of alunos){                           //Iteração da lista de alunos
+            json.result.push({                              //Adiciona cada aluno a chave result do objeto
                 id: aluno.id,
                 nome: aluno.nome,
                 sobrenome: aluno.sobrenome,
@@ -16,24 +16,24 @@ module.exports = {
                 email: aluno.email
             })
         }
-        response.status(200).json(json)
+        response.status(200).json(json)                     //Respondendo o codigo de status e envia o json com os dados em formato json
     },
 
     deleteAluno: async (request,response) => {
         let json ={error: "", result: {}}
 
-        let id = request.params.id
+        let id = request.params.id                          //Obtem os parametros id da URL
 
-        if(id){
-            let alunoValid = await alunoService.getAlunoById(id)
+        if(id){                                             //Se o id válido
+            let alunoValid = await alunoService.getAlunoById(id) //Busca o aluno de acordo com o ID
 
-            if(alunoValid.length == 0){
+            if(alunoValid.length == 0){                     //Se o aluno não for encontrado
                 json.error = "Aluno não encontrado!"
                 response.status(404).json(json)
-            }else{
-                await alunoService.deleteAluno(id)
+            }else{                                          //Caso o aluno seja encontrado
+                await alunoService.deleteAluno(id)          //Exclusão do aluno
 
-                json.result = `Aluno ${alunoValid[0].nome} excluido com sucesso`
+                json.result = `Aluno ${alunoValid[0].nome} excluido com sucesso` 
                 
                 response.status(200).json(json)
             }
@@ -73,4 +73,3 @@ module.exports = {
 
     
 }
-// id/nome/sobrenome/telefone/email
--- src/services/AlunoService.js
+++ src/services/AlunoService.js
@@ -1,15 +1,17 @@
 const {deleteAluno} = require('../controllers/AlunoController')
-const database = require('../database')
+const database = require('../database')                 //Importa a conexão com o banco de dados
 
-module.exports = {
+module.exports = {                                      //Exporta as funções para que possam ser usadas em outros arquivos
     readAlunos: () => {
-        return new Promise((resolve, reject) => {
-            database.query('SELECT * FROM aluno', (err,result) => {
-                if(err){
-                    reject(err)
+        return new Promise((resolve, reject) => {       //ReadAlunos retorna uma promise
+            database.query('SELECT * FROM aluno', (err,result) => { //Executa uma query sql para para selecionar todos os registros da tabela alunos
+                //err Captura qualquer erro que possa ocorrer ao executar a query
+                //result Contém o resultado da query se ela for executada com sucesso
+                if(err){                                //Se ouver um erro
+                    reject(err)                         //Promise rejeitada
                     return
-                }
-                resolve(result)
+                }                                       //Promise bem sucedida
+                resolve(result)                         //Resolvida com o resultado da consulta (lista de alunos)
             })
         })
     },
